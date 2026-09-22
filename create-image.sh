6bfeec54a9dd2c06b480c74f88a9485cec8b73a8
PonderForge <yotoby61@gmail.com>
2026-09-22 19:42:54 -0400

    update configs, remove vscode garbage, finish image creation rough draft

--- create-image.sh
+++ create-image.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Export compile variables
-ARCH=arm64
-CROSS_COMPILE=aarch64-linux-gnu-
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-gnu-
 
 
 # Create Build directories
@@ -18,4 +18,60 @@ make -j$(nproc) olddefconfig
 make -j$(nproc)
 make INSTALL_MOD_PATH=../build/root modules_install
 cp arch/arm64/boot/Image ../build/boot/kernel8.img 
+cp arch/arm64/boot/dts/broadcom/bcm2712-rpi-5-b.dtb ../build/boot/
 cd ..
+
+# Copy firmware files
+cd firmware
+cp boot/{start.elf,fixup.dat} ../build/boot/
+cp -r boot/overlays ../build/boot
+cd ..
+cp device_configs/{cmdline.txt,config.txt} build/boot
+
+# Compile Busybox 
+cp compile_configs/busybox_config busybox/.config
+cd busybox 
+make -j$(nproc)
+make INSTALL_PATH=../build/root install
+cd ..
+
+# Prep RootFS
+cd build/root
+mkdir -p etc/init.d proc sys dev tmp var/log 
+chmod 777 tmp
+sudo mknod dev/console c 5 1
+cd ../..
+
+cp device_configs/inittab build/root/etc
+cp device_configs/rcS build/root/etc/init.d
+cp /usr/aarch64-linux-gnu/lib/* build/root/lib
+
+# Create Image 
+dd if=/dev/zero of=data.img bs=1M count=512
+(
+echo o # Create a new empty DOS partition table
+echo n # Add a new partition
+echo p # Primary partition
+echo 1 # Partition number
+echo 2048  # First sector
+echo +100M  # Last sector
+echo t  # Set type
+echo c  # Set to FAT32
+echo n
+echo p 
+echo 2
+echo 
+echo 
+echo w # Write changes
+) | sudo fdisk ./data.img
+sudo losetup -fP ./data.img  # Mounts as /dev/loopX  
+sudo mkfs.vfat /dev/loopXp1  # Boot partition  
+sudo mkfs.ext4 /dev/loopXp2
+sudo mount /dev/loopXp1 /mnt
+sudo cp build/boot/* /mnt
+sudo umount /mnt
+sudo mount /dev/loopXp2 /mnt
+sudo cp build/root/* /mnt
+sudo umount /mnt
+
+echo "Now go forth and conquer in the name of Jesus Christ our Lord"
